774b9258e7c9345fc39d71f39cce40812e8a2f03
vanshdeep-15 <VanshdeepSingh@edu.sait.ca>
2025-11-21 18:46:09 -0700

    Replace Index component with HomeScreen component

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,3 @@
-import { Text, View } from "react-native";
+import HomeScreen from './HomeScreen';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+export default HomeScreen;
