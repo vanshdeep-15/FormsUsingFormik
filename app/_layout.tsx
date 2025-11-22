23f6a412634b3cc7f33fd940980834ae98015198
vanshdeep-15 <VanshdeepSingh@edu.sait.ca>
2025-11-21 18:49:16 -0700

    Refactor RootLayout to include multiple Stack screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Employee Management System' }} />
+      <Stack.Screen name="EmployeeForm" options={{ title: 'Employee Form' }} />
+      <Stack.Screen name="SignInForm" options={{ title: 'Sign In' }} />
+      <Stack.Screen name="SignUpForm" options={{ title: 'Sign Up' }} />
+    </Stack>
+  );
 }
